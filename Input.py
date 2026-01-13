d8be7605a0177c6f46b9fe501d330ba0469e08c2
dangerousCoder-OP <102338123+dangerousCoder-OP@users.noreply.github.com>
2026-01-13 23:38:42 +0530

    Initial commit

---
+++ Input.py
@@ -0,0 +1,15 @@
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+is_male = input("Enter your is_male: ")
+weight = input("Enter your weight: ")
+
+# But here is the problem that it is in string form to tackle this issue we can
+# directly typecast the input or after taking input we can typecast in the same variable
+print(name)
+print(age)
+print(is_male)
+print(weight)
+
+# age = int(input("Enter your age: "))
+# is_male = boolean(input("Enter your is_male: "))
+# weight = float(input("Enter your weight: "))
