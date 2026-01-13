1316156acbc5f28e59f140b195424156063d67fb
dangerousCoder-OP <102338123+dangerousCoder-OP@users.noreply.github.com>
2026-01-13 23:45:40 +0530

    Initial commit

---
+++ Ifelse.py
@@ -0,0 +1,17 @@
+age = 45
+if age < 18:
+    print("You cannot vote")
+else:
+    print("You have right to vote")
+
+marks = 75
+if marks >= 90:
+    print("A grade")
+elif marks >= 80:
+    print("B grade")
+elif marks >= 70:
+    print("C grade")
+elif marks >= 60:
+    print("D grade")
+else:
+    print("Fail")
