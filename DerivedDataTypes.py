3645f92d598b7bec7a9b0600fa81664a8890d2cd
dangerousCoder-OP <102338123+dangerousCoder-OP@users.noreply.github.com>
2026-01-15 12:14:25 +0530

    Initial commit

---
+++ DerivedDataTypes.py
@@ -0,0 +1,8 @@
+list1 = [1, 2, 3]
+tuple1 = (1, 2, 3)
+set1 = {1, 2, 3}
+dict1 = {1:90, 2:91}
+print(list1)
+print(tuple1)
+print(set1)
+print(dict1)
--- Type.py
+++ Type.py
@@ -3,14 +3,31 @@ age = 67
 is_male = True
 weight = 103.5
 
+list1 = [1, 2, 3]
+tuple1 = (1, 2, 3)
+set1 = {1, 2, 3}
+dict1 = {1:90, 2:91}
+
 # To know the type of the variables With Variables
 print(type(name))
 print(type(age))
 print(type(is_male))
 print(type(weight))
 
+
+print(type(list1))
+print(type(tuple1))
+print(type(set1))
+print(type(dict1))
+
 # Without using variables
 print(type(90))
 print(type("Norman"))
 print(type(True))
 print(type(90.8))
+
+
+print(type([1, 2, 3]))
+print(type({1, 2, 3}))
+print(type((1, 2)))
+print(type({1:90, 2:91}))
